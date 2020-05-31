22712248bd1ba886a7d1bd464076b6dcb0effea5
msaaddev <mrsaadirfan@gmail.com>
2020-05-31 16:20:06 +0500

    📖 DOC: comments

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// importing axios in the file
 const axios = require('axios');
 
+// an asychronous function to make the API call
 (async () => {
     await axios
         .get('https://api.ipgeolocationapi.com/countries')
         .then(({ data }) => console.log(data))
         .catch((err) => console.log(err));
 })();
+
